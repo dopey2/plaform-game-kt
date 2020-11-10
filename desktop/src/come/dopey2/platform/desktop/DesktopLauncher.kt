855c4897e49ef70b0a8cba073f1d48030ba83c07
dopey2 <daniel.gabor@supertich.fr>
2020-11-10 19:21:57 +0100

    rename package, change java to kotlin file

---
+++ desktop/src/come/dopey2/platform/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.dopey2.platform.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.dopey2.platform.Game
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(Game(), config)
+    }
+}
