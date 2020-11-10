efa8fe391675a5a67f67092043ed2093744275a7
dopey2 <daniel.gabor@supertich.fr>
2020-11-10 19:14:28 +0100

    first commit

---
+++ desktop/src/come/dopey2/platform/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package come.dopey2.platform.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import come.dopey2.platform.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
